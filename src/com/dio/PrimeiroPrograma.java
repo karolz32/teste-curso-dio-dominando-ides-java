18534e41254db6f65170f54a7450789219cc8fad
karolz32 <karoline.lima32@gmail.com>
2021-11-19 16:13:55 -0300

    first commit

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -6,7 +6,9 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		
-		Gato gato = new Gato(null, null, null);
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
 		
 		/*int a = 2;
 		int b = 3;
